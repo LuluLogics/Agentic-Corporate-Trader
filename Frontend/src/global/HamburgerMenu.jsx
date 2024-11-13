e43f2f9a05d43d4b65f073e45175a40ade4d70e8
KaralisKB <kristianbariss@gmail.com>
2024-11-13 09:41:49 +0000

    name fix 2

--- Frontend/src/global/HamburgerMenu.jsx
+++ Frontend/src/global/HamburgerMenu.jsx
@@ -402,7 +402,7 @@ const HamburgerMenu = () => {
                     fontWeight="bold"
                     sx={{ m: "10px 0 0 0" }}
                   >
-                    {user?.firstName + " " + user?.lastName || "User"}
+                    {user?.firstName && user?.lastName ? `${user.firstName} ${user.lastName}` : "User"}
                   </Typography>
                   <Typography variant="h5" color={colors.greenAccent[100]}>
                     Elite Investor
